0ac9d595ffcf783ce1aececc7a08b707748050ec
Marcela Mosquera <93941217+marcela-mosquera-e@users.noreply.github.com>
2024-07-04 18:24:25 -0500

    Update RegisterTest.java

--- src/test/java/ec/edu/epn/calidadsw/tests/RegisterTest.java
+++ src/test/java/ec/edu/epn/calidadsw/tests/RegisterTest.java
@@ -14,20 +14,16 @@ public class RegisterTest extends BaseTest {
     @Test (testName = "TC1-Register New Customer", description = "Registrar un cliente con todos los datos correctos")
     public void registerNewCustomer() {
         // Navega a la página para crear un nuevo cliente
-        homePage.goToCreateNewCustomerLink();
 
         // Rellena el formulario de registro con datos
-        registerPage.fillOutRegisterFormWithFakeData();
 
         // Haz clic en el botón de registro
-        registerPage.clickRegister();
 
         // Espera hasta que aparezca un mensaje de éxito después del registro
         WebElement msg = new WebDriverWait(driver, Duration.ofSeconds(Variables.TIME_OUT))
                 .until(ExpectedConditions.presenceOfElementLocated(registerPage.msgVerify));
 
         // Verifica que el mensaje obtenido coincida con el título esperado
-        Assert.assertEquals(msg.getText(), registerPage.expected_title);
     }
 
 }
